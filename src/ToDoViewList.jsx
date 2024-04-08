fae28a1aafbcc09c3f0e3b1e86e94383112c8ec6
sunnyshinde123 <sunnyshinde157@gmail.com>
2024-04-08 14:22:06 +0530

    commited now

--- src/ToDoViewList.jsx
+++ src/ToDoViewList.jsx
@@ -1,17 +1,12 @@
 import { useState } from "react"
 
 export default function ToDoViewList({listTask, onClick}){
-    // let [use, setUse]=useState([{}]);
-
-    // let deleteList= (id) =>{
-    //     setUse((prev)=> use.filter((prev)=> (prev.id!=id)));
-    // }
 
     return(
         <>
         <ul>
             {listTask.map((task)=> <li key={task.id}><span>{task.task}</span>
-            <button onClick={onClick}>Delete</button>
+            <button onClick={()=>onClick(task.id)}>Delete</button>
             </li>) }
         </ul>
         </>
