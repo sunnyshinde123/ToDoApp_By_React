2564cf57f7f5713629b71c3f5a33dcab27fb5cca
sunnyshinde123 <sunnyshinde157@gmail.com>
2024-04-08 15:21:52 +0530

    Up to Date

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -4,20 +4,64 @@ import ToDoViewList from "./ToDoViewList"
 import { v4 as uuidv4 } from 'uuid';
 
 export default function ToDoApp(){
-    let [listTask, setListTask]=useState([{id:uuidv4(), task:"Wake Up"}]);
+    let [listTask, setListTask]=useState([{id:uuidv4(), task:"Wake Up", isDone:true}]);
 
     let addTaskInList = (list) =>{
-        setListTask([...listTask, {id:uuidv4(), task:list}])
+        setListTask([...listTask, {id:uuidv4(), task:list, isDone:true}])
     }
 
     let deleteList = (id) =>{
         setListTask((prev)=>{ return listTask.filter((prev)=> prev.id!=id)} )
     }
 
+    let deleteAll = () =>{
+        setListTask((prev)=> listTask.filter((prev)=> {}));
+    }
+
+    let upperCaseAll = () =>{
+        setListTask((prev)=> listTask.map((prev)=> {
+            return {...prev, task:prev.task.toUpperCase()}
+        }))
+    }
+
+    let upperCase = (id) =>{
+        setListTask((prev)=> listTask.map((prev)=>{
+            if(prev.id==id){
+                return{
+                    ...prev,
+                    task:prev.task.toUpperCase()
+                }
+            }else{
+                return prev;
+            }
+        }))
+    }
+
+    let markAllDone = () =>{
+        setListTask((prev)=> listTask.map((prev)=>{
+            console.log(prev);
+            return {...prev, isDone:false}
+        }))
+    }
+
+    let markDone = (id) =>{
+        setListTask((prev)=> listTask.map((prev)=>{
+            if(prev.id==id){
+                return {
+                    ...prev,
+                    isDone:false,
+                }
+            }else{
+                return prev
+            }
+        }))
+    }
+
     return(
         <>
         <ToDoAddList addTaskInList={addTaskInList}/>
-        <ToDoViewList listTask={listTask}/>
+        <ToDoViewList listTask={listTask} deleteTask={deleteList} deleteAll={deleteAll} upperCaseAll={upperCaseAll} upperCase={upperCase} markAllDone={markAllDone} markDone={markDone}/>
+
         </>
     )
 }
--- src/ToDoViewList.jsx
+++ src/ToDoViewList.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
 
-export default function ToDoViewList({listTask, onClick}){
+export default function ToDoViewList({listTask, deleteTask, deleteAll, upperCaseAll, upperCase, markAllDone, markDone}){
 
     return(
         <>
         <ul>
-            {listTask.map((task)=> <li key={task.id}><span>{task.task}</span>
-            <button onClick={()=>onClick(task.id)}>Delete</button>
+            {listTask.map((task)=> <li key={task.id}><span style={task.isDone ?  {} : {textDecoration:"line-through"}}>{task.task}</span>
+            <button onClick={()=>deleteTask(task.id)}>Delete</button> &nbsp; &nbsp; <button onClick={()=>upperCase(task.id)}>UpeerCase</button> <button onClick={()=> markDone(task.id)}>Mark Done</button>
             </li>) }
         </ul>
+        <hr />
+        <button onClick={deleteAll}>Delete All</button>
+        <button onClick={upperCaseAll}>UpperCase All</button>
+        <button onClick={markAllDone}>Mark All Done</button>
         </>
     )
 }
